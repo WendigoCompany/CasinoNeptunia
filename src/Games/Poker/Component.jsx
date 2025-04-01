c87ff1a483edab6a9ba236b971f5a148b3c7d781
Wendigo <cosrikura@gmail.com>
2025-04-01 12:41:40 -0300

    Terminado , escalas de poker

--- src/Games/Poker/Component.jsx
+++ src/Games/Poker/Component.jsx
@@ -1,3 +1,4 @@
+import { countRepeat, SortMayor, SortMenor } from "../../Tools/tools";
 import Cards_Assets from "../Card_Assets"
 import random from "../random"
 
@@ -26,84 +27,255 @@ export default function Poker() {
         {
             name: 'High Card', vali: (HAND) => {
 
-                const specialValues = () => {
-                    switch (key) {
-                        case 10:
-
-                            break;
-
-                        case 11:
-
-                            break;
-                        case 12:
-
-                            break;
 
 
-                    }
-                }
+                // let RV = Raw_Values(HAND);
+                // if (RV.indexOf(1) != -1) {
+                //     return { bool: true, scalevalue: 14, scalename: 'High Card - AS', id: 0 };
+                // } else {
+                //     RV = RV.sort()
 
-                let RV = Raw_Values(HAND);
-                if (RV.indexOf(1) != -1) {
-                    return { bool: true, scalevalue: 14, scalename: 'High Card - AS', id: 0 };
-                } else {
-                    RV = RV.sort()
+                //     return { bool: true, scalevalue: RV[0], scalename: 'High Card -' + specialValues(RV[0]), id: 0 };
 
-                    return { bool: true, scalevalue: RV[0], scalename: 'High Card -' + , id: 0 };
+                // }
+                let RV = SortMayor(ReplaceAs(Raw_Values(HAND)));
 
-                }
-
+                return { bool: true, scalevalue: (RV[0]), scalename: 'High Card -' + specialValues(RV[0]), id: 0 };
 
             }, id: 0
         },
         {
             name: 'One Pair', vali: (HAND) => {
 
+                let card_repeated = false;
+                let RV = SortMenor(ReplaceAs(Raw_Values(HAND)));
+
+                for (let i = 0; i < RV.length; i++) {
+                    if (countRepeat(RV, RV[i]) == 2) {
+                        card_repeated = RV[i]
+                    }
+
+                }
+
+                if (card_repeated) {
+                    return { bool: true, scalevalue: card_repeated, scalename: 'Pair of ' + specialValues(card_repeated), id: 1 };
+
+                } else {
+                    return { bool: false };
+
+                }
+
             }, id: 1
         },
         {
             name: 'Two Pair', vali: (HAND) => {
+                let card_repeated = [];
+                let RV = SortMenor(ReplaceAs(Raw_Values(HAND)));
+
+                const noRepeat = () => {
+
+
+                    if (card_repeated.length == 4) {
+                        return [card_repeated[0], card_repeated[2]]
+                    }
+                    return []
+
+                }
+
+                for (let i = 0; i < RV.length; i++) {
+                    if (countRepeat(RV, RV[i]) == 2) {
+                        card_repeated.push(RV[i])
+                    }
+
+                }
+                card_repeated = noRepeat(SortMenor(card_repeated))
+                if (card_repeated.length == 2) {
+                    return { bool: true, scalevalue: card_repeated.toString().replace(",", "-"), scalename: 'Double Pair - ' + `${specialValues(card_repeated[0])} and ${specialValues(card_repeated[1])}`, id: 2 };
+                } else {
+                    return { bool: false };
+                }
 
             }, id: 2
         },
         {
             name: 'Trio', vali: (HAND) => {
+                let card_repeated = false;
+                let RV = SortMenor(ReplaceAs(Raw_Values(HAND)));
 
+                for (let i = 0; i < RV.length; i++) {
+                    if (countRepeat(RV, RV[i]) == 3) {
+                        card_repeated = RV[i]
+                    }
+
+                }
+
+                if (card_repeated) {
+                    return { bool: true, scalevalue: card_repeated, scalename: 'Trio of ' + specialValues(card_repeated), id: 3 };
+
+                } else {
+                    return { bool: false };
+
+                }
             }, id: 3
         },
         {
             name: 'Straight', vali: (HAND) => {
+                let RV1 = SortMenor(Raw_Values(HAND));
+                let RV2 = SortMenor(ReplaceAs(Raw_Values(HAND)));
+                let RV1_min = RV1[0];
+                let RV2_min = RV2[0];
+                let scaleToUse = RV1
+                let validator = true
 
+
+                for (let i = 1; i < RV1.length; i++) {
+                    if (RV1[i] != RV1_min + i) {
+                        validator = false
+                        break
+                    }
+                }
+
+
+                if (!validator) {
+                    scaleToUse = RV2
+                    validator = true
+                    for (let i = 1; i < RV2.length; i++) {
+                        if (RV2[i] != RV2_min + i) {
+                            validator = false
+                            break
+                        }
+                    }
+
+                }
+
+
+                if (validator) {
+                    return { bool: true, scalevalue: scaleToUse[scaleToUse.length - 1], scalename: 'Straight - ' + `${scaleToUse[0]} to ${scaleToUse[scaleToUse.length - 1]}`, id: 4 };
+                } else {
+                    return { bool: false }
+                }
             }, id: 4
         },
         {
             name: 'Flush', vali: (HAND) => {
+                const palo = HAND[0].palo;
+                for (let i = 0; i < HAND.length; i++) {
+                    if (HAND[i].palo != palo) {
+                        return { bool: false };
+                    }
+
+                }
+
+                return { bool: true, scalevalue: Scale[0].vali(HAND).scalevalue, scalename: 'Flush of ' + specialValues(palo), id: 5 };
 
             }, id: 5
         },
         {
             name: 'Full House', vali: (HAND) => {
 
+                const pair = Scale[1].vali(HAND);
+                const trio = Scale[3].vali(HAND);
+                if (pair.bool && trio.bool) {
+                    return { bool: true, scalevalue: `${pair.scalevalue}-${trio.scalevalue}`, scalename: 'Full House - ' + `${specialValues(pair.scalevalue)} and ${specialValues(trio.scalevalue)}`, id: 6 };
+                } else {
+                    return { bool: false };
+                }
             }, id: 6
         },
         {
             name: 'Poker', vali: (HAND) => {
+                let card_repeated = false;
+                let RV = SortMenor(ReplaceAs(Raw_Values(HAND)));
 
+                for (let i = 0; i < RV.length; i++) {
+                    if (countRepeat(RV, RV[i]) == 4) {
+                        card_repeated = RV[i]
+                    }
+
+                }
+
+                if (card_repeated) {
+                    return { bool: true, scalevalue: card_repeated, scalename: 'Poker of ' + specialValues(card_repeated), id: 7 };
+
+                } else {
+                    return { bool: false };
+
+                }
             }, id: 7
         },
         {
-            name: 'Straight Flush', vali: (HAND) => {
+            name: 'Straight Flush', vali: (HAND) => {                
+                const flush = Scale[5].vali(HAND);
+                const straight = Scale[4].vali(HAND);
+                if(flush.bool && straight.bool){
+            
+                    return { bool: true, scalevalue: straight.scalevalue, scalename: 'Straight Flush - ' + `${straight.scalename.split("-")[1]}`, id: 8 };
+                }
+                
+                
+                return { bool: false };
 
             }, id: 8
         },
         {
             name: 'Royal Flush', vali: (HAND) => {
 
+                const flush = Scale[5].vali(HAND);
+                let RV = SortMenor(ReplaceAs(Raw_Values(HAND)));
+                if (flush.bool && RV.indexOf(10) != -1 && RV.indexOf(11) != -1 && RV.indexOf(12) != -1 && RV.indexOf(13) != -1 && RV.indexOf(14) != -1) {
+                    return { bool: true, scalevalue: 14, scalename: 'Royal Flush', id: 9 };
+                }
+                return { bool: false };
             }, id: 9
         },
     ];
 
+    const ReplaceAs = (HAND) => {
+        for (let i = 0; i < HAND.length; i++) {
+            HAND[i] = isAs(HAND[i])
 
+        }
+
+        return HAND
+    }
+
+    const isAs = (card) => {
+        if (card == 1) {
+            return 14
+        } else {
+            return card
+        }
+    }
+
+
+    const specialValues = (card) => {
+        switch (card) {
+            case 14:
+                return "AS"
+            case 11:
+                return "JACK"
+
+            case 12:
+                return "QUEEN"
+
+            case 13:
+                return "KING"
+
+            case "C":
+                return "Hearts"
+            case "T":
+                return "Clubs"
+            case "D":
+                return "Diamonds"
+            case "P":
+                return "Spades"
+
+            default:
+                return card
+
+
+        }
+    }
 
 
     //FUNCIONES DEL DECK
@@ -163,8 +335,18 @@ export default function Poker() {
 
 
 
+        HAND = [
+            { val: 11, palo: 'T' },
+            { val: 10, palo: 'T' },
+            { val: 12, palo: 'T' },
+            { val: 1, palo: 'T' },
+            { val: 13, palo: 'T' },
 
-        Scale[0].vali(HAND)
+        ]
+
+        console.log(Scale[9].vali(HAND));
+
+        //usar unshift
 
     }
 
@@ -270,6 +452,8 @@ del juego hasta que se inicie una nueva mano. Sin embargo, algunos grupos o vari
 
     //START
 
+    
+ //SI 2 ROYAL FLUSH, GANA EL JUGADOR
     if (ORIGINAL_DECK.length == 0) {
         for (let i = 0; i < 12; i++) {
             ORIGINAL_DECK.push({
